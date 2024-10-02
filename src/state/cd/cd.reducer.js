d2fcdd8578ede9f25637a92542782b7a5a67ddad
Hennie Scheepers <hennie.104@hotmail.com>
2024-10-02 12:58:24 +0200

    NE-681: remove id assignment

--- src/state/cd/cd.reducer.js
+++ src/state/cd/cd.reducer.js
@@ -3,12 +3,11 @@ import { ADD_CD, DELETE_CD, UPDATE_CD } from './cd.action';
 const initialState = {
   cds: []
 };
-let next = 1;
 
 const cdReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_CD:
-      return { ...state, cds: [...state.cds, { ...action.payload, id: next++}] };
+      return { ...state, cds: [...state.cds, { ...action.payload }] };
 
     case UPDATE_CD:
       return {

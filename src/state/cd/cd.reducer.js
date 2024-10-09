996bf769f8a775bf27a66df8a31857690f8b06df
Hennie Scheepers <hennie.104@hotmail.com>
2024-10-09 10:36:42 +0200

    NE-670: add saga for cd edit

--- src/state/cd/cd.reducer.js
+++ src/state/cd/cd.reducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { deleteItem, updateItem } from '../../util/state-util';
+import { addItem, deleteItem, updateItem } from '../../util/state-util';
 
 const initialState = {
   cds: [],
@@ -8,25 +8,34 @@ const initialState = {
   error: null
 };
 
-let nextID = 1;
-
 const cdSlice = createSlice({
   name: 'cdSlice',
   initialState,
   reducers: {
-    setCD: (state, action) => {},
-    addCD: (state, action) => {
-      state.cds.push({ ...action.payload, id: nextID++ });
-      setLoading(false);
-    },
     setLoading: (state, action) => {
       state.loading = action.payload;
     },
-    updateCD: (state, action) => updateItem(state.cds, action.payload),
+
+    setCD: () => {
+      setLoading(true);
+    },
+    addCD: (state, action) => {
+      addItem(state.cds, action.payload);
+      setLoading(false);
+    },
+
+    editCD: () => {
+      setLoading(true);
+    },
+    updateCD: (state, action) => {
+      updateItem(state.cds, action.payload);
+      setLoading(false);
+    },
+
     deleteCD: (state, action) => deleteItem(state.cds, action.payload)
   }
 });
 
-export const { setCD, addCD, setLoading, updateCD, deleteCD } = cdSlice.actions;
+export const { setCD, addCD, setLoading, editCD, updateCD, deleteCD } = cdSlice.actions;
 
 export default cdSlice.reducer;
--- src/state/media-catalogue.store.js
+++ src/state/media-catalogue.store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 
+import cdSaga from './cd/cd.saga';
+
 import bookReducer from './book/book.reducer';
 import cdReducer from './cd/cd.reducer';
 import dvdReducer from './dvd/dvd.reducer';
-import cdSaga from './cd/cd.saga'
 
 const sagaMiddleware = createSagaMiddleware();
 

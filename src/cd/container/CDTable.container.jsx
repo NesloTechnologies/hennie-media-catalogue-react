a74db68f08ce4d1514dd83fafaba810a4c41edb3
Hennie Scheepers <hennie.104@hotmail.com>
2024-10-10 10:41:39 +0200

    NE-710: add cd delete saga

--- src/cd/container/CDTable.container.jsx
+++ src/cd/container/CDTable.container.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { deleteCD, loadCDsTrigger } from '../../state/cd/cd.reducer';
+import { deleteCDTrigger, loadCDsTrigger } from '../../state/cd/cd.reducer';
 
 import CDTable from '../table/CDTable';
 
@@ -15,7 +15,7 @@ const CDTableContainer = () => {
   const cds = useSelector(({ cdStore }) => cdStore.cds);
 
   const removeCD = (id) => {
-    dispatch(deleteCD(id));
+    dispatch(deleteCDTrigger(id));
   };
 
   return <CDTable cds={cds} removeCD={removeCD} />;

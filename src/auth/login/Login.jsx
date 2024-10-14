161f533fc24b26ae38aff0ef7494c336d49d3b79
Hennie Scheepers <hennie.104@hotmail.com>
2024-10-14 10:58:12 +0200

    NE-704: add login view

---
+++ src/auth/login/Login.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import handleChange from '../../util/handle-change';
+
+import '../../stylesheet/form.scss';
+
+const Login = () => {
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  });
+
+  return (
+    <section className="form auth">
+      <header>
+        <h1>Login</h1>
+      </header>
+
+      <form>
+        <fieldset>
+          <fieldset>
+            <label htmlFor="username">Username</label>
+            <input
+              name="username"
+              id="username"
+              type="text"
+              value={user.username}
+              onChange={(event) => handleChange(event, setUser)}
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="password">Password </label>
+            <input
+              name="password"
+              id="password"
+              type="password"
+              value={user.password}
+              onChange={(event) => handleChange(event, setUser)}
+            />
+          </fieldset>
+        </fieldset>
+
+        <fieldset>
+          <button>Login</button>
+
+          <Link to="/register">Register</Link>
+        </fieldset>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
